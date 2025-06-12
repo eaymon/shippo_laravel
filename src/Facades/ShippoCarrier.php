b57c47c2c00e070dca6ee31ad60b27b33c3aab55
Christian Martin Cabucos <christianmartincabucos@192.168.1.11>
2025-06-13 02:19:45 +0800

    Update ShippoCarrier facade methods for clarity and functionality

--- src/Facades/ShippoCarrier.php
+++ src/Facades/ShippoCarrier.php
@@ -5,10 +5,15 @@ namespace FarmToYou\ShippoLaravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static object getCarriers(array|null $carriers = null, bool $forceRefresh = false)
+ * @method static object getCarriersOnly($carriers = null, $forceRefresh = false)
+ * @method static object getCarriersServiceLevels(array|null $carriers = null, bool $forceRefresh = false)
  * @method static object|null getServiceLevel(string $serviceCode)
  * @method static bool clearCache()
  * @method static array getFormattedForSelect(string $groupBy = 'carrier')
+ * @method static array getRates(array $shipment)
+ * @method static object validateAddress(array $address)
+ * @method static object createLabel(array $transaction)
+ * @method static object trackShipment(string $trackingNumber, string|null $carrier = null)
  * 
  * @see \FarmToYou\ShippoLaravel\ShippoCarrier
  */
